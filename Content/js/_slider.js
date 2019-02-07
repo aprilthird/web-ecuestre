36dcd1736b5f3f51a16c06bb2c0b9d40de9c2253
Samuel <ext.secure@outlook.com>
2019-02-06 21:14:02 -0500

    cleanuop

--- Content/js/_slider.js
+++ Content/js/_slider.js
@@ -1,20 +1,16 @@
-﻿function $(root, el) {
-  if(!root)
-    return [];
-  return root.querySelector(el);
+﻿'use strict';
+function $(selector, context) {
+  return (context || document).querySelectorAll(selector);
 }
-function find(root, el) {
-  if(!root)
-    return [];
-  return root.querySelectorAll(el); 
+function $1(selector, context) {
+  return (context || document).querySelector(selector);
 }
-
 (function(slider, caption) {
-  let inputs = find($(document, slider), 'input');
-  let caps = find($(document, caption), '.slider3d-caption__text');
+  let inputs  = $('input', $1(slider));
+  let caps    = $('.slider3d-caption__text',$1(caption));
   if (inputs.length < 1 || caps.length < 1)
     return;
-  
+
   let id;
   for(let i = 0; i < inputs.length; i++) {
     id = caps[i].id;
@@ -23,13 +19,13 @@ function find(root, el) {
      
     inputs[i].setAttribute('data-tooltip',`#${id}`);
   }
+  $1('.slider3d-caption .is-active').style.display = "block";
   inputs.forEach(function(input) {
     input.addEventListener('change', function() {
-      let cap = $(document, '.slider3d-caption .is-active');
-      if(cap) {
-        cap.classList.toggle('is-active');
-      }
-      $(document, this.dataset.tooltip).classList.add('is-active');
+      $1('.slider3d-caption .is-active').style.display = "none";
+      $1('.slider3d-caption .is-active').classList.remove("is-active");
+      $1(this.dataset.tooltip).classList.add('is-active');
+      $1(this.dataset.tooltip).style.display = "block";
     });
   });
 
